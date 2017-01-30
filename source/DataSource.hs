85ae1f220f495aaf6368542f3963f7dec8fe5077
Michael Mounteney <jackrose@landcroft.com>
2017-01-30 20:43:19 +1000

    Tidy-up of connection-opening code.

--- source/DataSource.hs
+++ source/DataSource.hs
@@ -1,5 +1,5 @@
 {-|
-Description: Management of user-defined data sources;  to be repealed.
+Description: Management of user-defined data sources;  to be repealed.  Only retained for how enSerialise might work
 Copyright: (c) Michael Mounteney, 2015
 License: BSD 3 clause
 Maintainer: the project name, all lower case, at landcroft dot com
@@ -13,7 +13,7 @@ Data sources are the content of question/answer pairs.  They can be SQL tables, 
 {-# LANGUAGE OverloadedStrings #-}
 
 
-module DataSource (DataSource(..), DataVariant(..), deSerialise) where
+module DataSource (enSerialise) where
 
 
 import TextShow (showt)
@@ -22,11 +22,9 @@ import qualified TextList as DL (enSerialise, deSerialise)
 import MaybeIntValue (maybeIntValue)
 
 
-data DataSource = DataSource DT.Text DT.Text DataVariant
-
 -- | All the details to identify a data source without actually opening a connection to it.
 data DataVariant
-	= Postgres { server :: DT.Text, port :: Maybe Int, database ::Maybe  DT.Text, namespace :: Maybe DT.Text, table :: DT.Text, username :: Maybe DT.Text, password :: Maybe DT.Text }
+	= Postgres { server :: DT.Text, port :: Maybe Int, database ::Maybe  DT.Text, table :: DT.Text, username :: Maybe DT.Text, password :: Maybe DT.Text }
 	| Sqlite3 { tableName :: DT.Text }
 	| CSV { separator :: Char, fileCSV :: DT.Text }
 	| XMLSource { fileXML :: DT.Text }
@@ -42,31 +40,7 @@ showMT = maybe DT.empty id
 
 -- TODO we don't need this;  replace with something that validates JSON input or similar.
 enSerialise :: DataVariant -> DT.Text
-enSerialise (Postgres serverIP portNo dbase nameSpace dtable userName passWord) = DL.enSerialise [ "P", serverIP, showMI portNo, showMT dbase, showMT nameSpace, dtable, showMT userName, showMT passWord ]
+enSerialise (Postgres serverIP portNo dbase nameSpace dtable userName passWord) = DL.enSerialise [ "P", serverIP, showMI portNo, showMT dbase, dtable, showMT userName, showMT passWord ]
 enSerialise (Sqlite3 dtableName) = DL.enSerialise [ "Q", dtableName ]
 enSerialise (CSV recseparator ffileCSV) = DL.enSerialise [ "C", DT.singleton recseparator, ffileCSV ]
 enSerialise (XMLSource ffileXML) = DL.enSerialise [ "X", ffileXML ]
-
-
-deSerialise :: DT.Text -> Maybe DataVariant
-deSerialise = deSerialise' . DL.deSerialise
-
-
-deSerialise' :: [DT.Text] -> Maybe DataVariant
-deSerialise' [ "P", serverIP, maybePort, maybeDBase, maybeNamespace, dtable, maybeUsername, maybePassword ] =
-		Just $ Postgres
-			serverIP
-			(maybeIntValue maybePort)
-			(if DT.null maybeDBase then Nothing else Just maybeDBase)
-			(if DT.null maybeNamespace then Nothing else Just maybeNamespace)
-			dtable
-			(if DT.null maybeUsername then Nothing else Just maybeUsername)
-			(if DT.null maybePassword then Nothing else Just maybePassword)
-deSerialise' [ "Q", dtableName ] = Just $ Sqlite3 dtableName
-deSerialise' [ "C", recseparator, ffileCSV ] =
-		if DT.length recseparator == 1 then
-			Just $ CSV (DT.head recseparator) ffileCSV
-		else
-			Nothing
-deSerialise' [ "X", ffileXML ] = Just $ XMLSource ffileXML
-deSerialise' _ = Nothing
--- source/LearningResync.hs
+++ source/LearningResync.hs
@@ -12,28 +12,26 @@ Data sources may be updated externally to JR.
 
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 
 module LearningResync (update) where
 
 
-import Data.Time (getCurrentTime, UTCTime)
+import Data.Time (getCurrentTime)
 import Control.Exception (catch)
 import Data.List (foldl', intersperse)
 import qualified Data.Map as DM (insert)
-import qualified Data.Text as DT (Text, concat, pack, unpack)
-import qualified Database.Persist.Class (Key)
+import qualified Data.Text as DT (Text, concat, pack, unpack, null)
 import Database.Persist.Sql (Entity)
-import Database.Persist.Sqlite (ConnectionPool, runSqlPool, selectList)
+import Database.Persist.Sqlite (runSqlPool, selectList)
 import Database.Persist (replace)
-import Database.Persist.Sql (insertUnique)
+import Database.Persist.Sql (insertBy)
 import Database.Persist.Types (entityKey, entityVal)
-import DataSource (deSerialise)
 import TextList (enSerialise)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (catMaybes)
 import qualified JRState (JRState(..), runFilteredLoggingT)
 import qualified LearningData (DataSource(..), DataRow(..), DataSourceId)
-import qualified DataSource as DS (DataVariant(..))
 import qualified Database.HDBC as HDBC (SqlError, SqlColDesc, describeTable, prepare, sExecute, sFetchAllRows)
 import Database.HDBC.Statement (Statement)
 import Database.HDBC.PostgreSQL (connectPostgreSQL)
@@ -42,6 +40,8 @@ import Control.Monad.Logger (LoggingT, logWarnNS)
 import Control.Monad.STM (atomically)
 import Control.Concurrent.STM.TVar (modifyTVar')
 import ConnectionSpec (DataDescriptor(..), DataHandle(..))
+import MaybeIntValue (maybeIntValue)
+import TextList (deSerialise)
 
 
 -- | Create handles or connections to all data sources;
@@ -63,22 +63,19 @@ type DataSchemes = [(LearningData.DataSourceId, DataDescriptor)]
 
 updateOneSource :: JRState.JRState -> LoggingT IO DataSchemes -> Entity LearningData.DataSource -> LoggingT IO DataSchemes
 
-updateOneSource site schemeMap sourceRecord = maybe badConnString mkConnection (deSerialise dataSourceString) where
+updateOneSource site schemeMap sourceRecord = liftIO (connection site (deSerialise dataSourceString)) >>= either badConnString reSyncRows where
 
-	badConnString = logWarnNS dataNameString (DT.concat [DT.pack "Invalid data source: ", dataSourceString]) >> schemeMap
-
-	mkConnection :: DS.DataVariant -> LoggingT IO DataSchemes
-	mkConnection connClass = liftIO (connection site connClass) >>= reSyncRows
+	badConnString word = logWarnNS dataNameString (DT.concat [dataSourceString, ": error: ", word]) >> schemeMap
 
 	reSyncRows :: OpenDataSource -> LoggingT IO DataSchemes
-	reSyncRows (Unavailable justification) = logWarnNS dataNameString justification >> schemeMap
-	reSyncRows (OpenDataSource openConn colheads priKeys) = liftIO getCurrentTime >>= updateSync
+	reSyncRows (OpenDataSource openConn colheads _ dataKeysList) = liftIO getCurrentTime >>= updateSync
 				>> schemeMap >>= liftIO . return . (:) (dataSourceKey, DataDescriptor colheads dataSourceKey openConn) where
-		updateSync timeStamp = liftIO (reSyncOneSource openConn site colheads timeStamp priKeys dataSourceKey learningPersistPool) >>= updateSource timeStamp
+		updateSync timeStamp = runSqlPool (sequence $ map insertRow dataKeysList) learningPersistPool >>= updateSource timeStamp where
+			insertRow keyValue = insertBy $ LearningData.DataRow keyValue dataSourceKey timeStamp
 		-- Update time-stamp only replace if new data_rows were inserted.
 		-- It looks like we could use just a Boolean, but the foldl' below stops when True be returned
-		updateSource _ 0 = return ()
-		updateSource timeStamp _ = runSqlPool (replace dataSourceKey dataSourceParts{LearningData.dataSourceResynced = timeStamp}) learningPersistPool
+		updateSource _ [] = return ()
+		updateSource timeStamp (_ : _) = runSqlPool (replace dataSourceKey dataSourceParts{LearningData.dataSourceResynced = timeStamp}) learningPersistPool
 
 	learningPersistPool = JRState.tablesFile site
 	dataSourceKey = entityKey sourceRecord
@@ -87,55 +84,59 @@ updateOneSource site schemeMap sourceRecord = maybe badConnString mkConnection (
 	dataNameString = LearningData.dataSourceName dataSourceParts
 
 
-data OpenDataSource = OpenDataSource DataHandle [DT.Text] [DT.Text] | Unavailable DT.Text
+-- fields are:  opened handle/connection field_list key_list row_keys
+data OpenDataSource = OpenDataSource DataHandle [DT.Text] [DT.Text] [DT.Text]
 
 
-reSyncOneSource :: DataHandle -> JRState.JRState -> [DT.Text] -> UTCTime -> [DT.Text] -> Database.Persist.Class.Key LearningData.DataSource -> ConnectionPool -> IO Int
+-- Given a data-source code, open it and return the required parameters.
+connection :: JRState.JRState -> [DT.Text] -> IO (Either DT.Text OpenDataSource)
 
-reSyncOneSource (Postgres sourceConn sourceDBtable) site _ timeStamp primaryKey sourceKey learningPersistPool =
-	HDBC.prepare sourceConn primyKeyQuery >>= exeStmt >>= foldl' row1yKeyUpdate (return 0) where
-		-- extract primary key value from row as [Maybe String] and convert to Maybe [SerialisedKey]
-		row1yKeyUpdate :: IO Int -> [Maybe String] -> IO Int
-		row1yKeyUpdate mAddCount dataRow = tryInsert mAddCount ((enSerialise . map DT.pack) `fmap` sequence dataRow)
-		-- Insert one key value into learning data, if it not already exist therein.
-		-- We don't log anything because the Persist calls do it all.
-		-- We accume a Boolean which is 'was anything changed?'
-		-- TODO try mapM insertBy [rows]
-		-- TODO do something useful if the key be Nothing
-		tryInsert :: IO Int -> Maybe DT.Text -> IO Int
-		tryInsert mAddCount = maybe mAddCount (tryInsertKey mAddCount)
-		tryInsertKey :: IO Int -> DT.Text -> IO Int
-		-- TODO could we return the error code rather than just throwing it away with const mAddCount?
-		tryInsertKey mAddCount keyValue = JRState.runFilteredLoggingT site (runSqlPool (insertUnique $ LearningData.DataRow keyValue sourceKey timeStamp) learningPersistPool)
-			>>= maybe mAddCount (\_ -> ((+) 1) `fmap` mAddCount)
-		-- SQL to select primary key data rows
-		primyKeyQuery = "SELECT " ++ primyKeysForQ ++ " FROM \"" ++ DT.unpack sourceDBtable ++ "\";"
-		-- comma-separated list of primary key fieldsKey
-		primyKeysForQ = DT.unpack $ DT.concat $ intersperse packedComma $ map enQuote primaryKey
+connection site [ "P", serverIP, maybePort, maybeDBase, dataTable, maybeUsername, maybePassword ] = catch (neoConn >>= pullStructure) sourceFail where
 
-
--- TODO merge this with DataSource.hs;  there's an unnecessary layer here.
-connection :: JRState.JRState -> DS.DataVariant -> IO OpenDataSource
-
-connection site (DS.Postgres serverIP maybePort dbase maybeTable dataTable maybeUser maybePassword) = catch (neoConn >>= pullStructure) sourceFail where
 	neoConn = connectPostgreSQL $
 			"host=" ++ DT.unpack serverIP ++ "" ++
-			maybe "" (makeLabel " port") maybePort ++
-			maybe "" (\db -> makeLabel " dbname" (DT.unpack db)) dbase ++
-			" user=" ++ (DT.unpack $ fromMaybe (JRState.databaseUser site) maybeUser) ++
-			maybe "" (\pw -> makeLabel " password" (DT.unpack pw)) maybePassword
+			maybe "" (makeLabel " port") (maybeIntValue maybePort) ++
+			(if DT.null maybeDBase then "" else makeLabel " dbname" (DT.unpack maybeDBase)) ++
+			" user=" ++ (DT.unpack $ if DT.null maybeUsername then JRState.databaseUser site else maybeUsername) ++
+			(if DT.null maybePassword then "" else makeLabel " password" (DT.unpack maybePassword))
+
 	pullStructure conn = HDBC.describeTable conn tableNameStr >>= mashIntoFields conn
+
 	mashIntoFields conn rows = HDBC.prepare conn primyKeyQuery >>=
 			exeStmt >>=
 			return . maybe [] (map DT.pack) . sequence . map head >>=
-			return . OpenDataSource (Postgres conn dataTable) (map putColHead rows)
-	sourceFail :: HDBC.SqlError -> IO OpenDataSource
-	sourceFail = return . Unavailable . DT.pack . show
+			kazam where
+
+			kazam primaryKey = HDBC.prepare conn primyKeyListQuery
+					>>= exeStmt
+					-- extract primary key value from row as [[Maybe String]] and convert to [DataRow]
+					-- TODO do something useful if the key be Nothing;  i.e., if any key field be Nothing
+					>>= return . map (enSerialise . map DT.pack) . catMaybes . map sequence
+					>>= return . Right . OpenDataSource (Postgres conn dataTable) (map putColHead rows) primaryKey where
+
+				-- SQL to select primary key data rows
+				primyKeyListQuery = "SELECT " ++ primyKeysForQ ++ " FROM \"" ++ DT.unpack dataTable ++ "\";"
+
+				-- comma-separated list of primary key fieldsKey
+				primyKeysForQ = DT.unpack $ DT.concat $ intersperse packedComma $ map enQuote primaryKey
+
+	sourceFail :: HDBC.SqlError -> IO (Either DT.Text OpenDataSource)
+	sourceFail err = return $ Left $ DT.concat ["SQL error: ", DT.pack $ show err]
+
 	primyKeyQuery = "SELECT \"" ++ attName ++ "\" FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey) WHERE i.indrelid = '\"" ++ tableNameStr ++ "\"'::regclass ORDER BY a.attnum;"
+
 	tableNameStr = DT.unpack dataTable
+
 	makeLabel label value = label ++ "=\"" ++ show value ++ "\""
 
-connection _ (DS.Sqlite3 dtableName) = return $ OpenDataSource (ConnectionSpec.Sqlite3 dtableName) [] []
+
+connection _site [ "Q", _dtableName ] = return $ Left $ DT.pack $ "SQLite not implemented yet"
+
+connection _site [ "C", _recseparator, _ffileCSV ] = return $ Left $ DT.pack $ "CSV not implemented yet"
+
+connection _site [ "X", _ffileXML ] = return $ Left $ DT.pack $ "XML not implemented yet"
+
+connection _ connStr = return $ Left $ DT.concat ("invalid connection string" : connStr)
 
 
 -- TODO actually look at the Integer returned and throw an error if necessary
