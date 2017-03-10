70bd6510da6ea3cb5e29693eb35399a89ceffdaa
Michael Mounteney <jackrose@landcroft.com>
2017-03-10 12:06:42 +1000

    Added framework for second executable:  data-conversion from Anki.

---
+++ source/AnkiConvert.hs
@@ -0,0 +1,48 @@
+{-|
+Module: Copy from an Anki database into JackRose.
+Description: It all starts here.
+Copyright: (c) Michael Mounteney, 2015
+License: BSD 3 clause
+Maintainer: the project name, all lower case, at landcroft dot com
+Stability: experimental
+Portability: undefined
+
+Because we re-use the start-up configuration framework for JR itself, there's no room for the
+Anki database name so it is hard-wired to "anki.sqlite".
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Main (main) where
+
+
+import qualified Authorisation (migrateData)
+import qualified LearningData (migrateData)
+import qualified DeckData (migrateData)
+import qualified Persistency (upgradeDB)
+import qualified CommandArgs (args)
+import qualified JRState (JRState(..))
+import qualified LearningResync (update)
+import SiteConfigFromFile (siteObject)
+import qualified Database.Persist.Sqlite as PerstQ (createSqlitePool, ConnectionPool)
+import AnkiSchema ()
+import LogFilter (runFilteredLoggingT)
+import Control.Monad.Logger (LogLevel(..))
+
+
+-- | Start here.
+main :: IO ()
+-- | Turn the command line arguments into an easily-queried object,
+-- use that to inform the construction of the foundation siteObject,
+-- perform any required data migrations,
+-- and hand over to Warp, to launch the service.
+main = CommandArgs.args >>= siteObject >>= letsGo
+
+
+letsGo :: JRState.JRState -> IO ()
+letsGo site =
+	mapM_ (Persistency.upgradeDB (JRState.tablesFile site)) [Authorisation.migrateData, LearningData.migrateData, DeckData.migrateData]
+	>> LearningResync.update site
+	>> (runFilteredLoggingT LevelInfo (PerstQ.createSqlitePool "anki.sqlite" 1) :: IO (PerstQ.ConnectionPool))
+	>> putStrLn "incomplete"
---
+++ source/AnkiSchema.hs
@@ -0,0 +1,79 @@
+{-|
+Description: Anki storage; read-only access to Anki database.
+Copyright: (c) Michael Mounteney, 2015
+License: BSD 3 clause
+Maintainer: the project name, all lower case, at landcroft dot com
+Stability: experimental
+Portability: undefined
+-}
+
+
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, FlexibleInstances, TypeFamilies, MultiParamTypeClasses, ExistentialQuantification, GeneralizedNewtypeDeriving, DeriveGeneric #-}
+
+
+module AnkiSchema where
+
+
+import qualified Yesod as Y
+import Data.Int (Int32)
+import Data.Text (Text)
+
+
+Y.share [Y.mkPersist Y.sqlSettings] [Y.persistLowerCase|
+TableColumn
+	crt	Int32 NOT NULL
+	mod	Int32 NOT NULL
+	scm	Int32 NOT NULL
+	ver	Int32 NOT NULL
+	dty	Int32 NOT NULL
+	usn	Int32 NOT NULL
+	ls	Int32 NOT NULL
+	conf	Text NOT NULL
+	models	Text NOT NULL
+	decks	Text NOT NULL
+	dconf	Text NOT NULL
+	tags	Text NOT NULL
+DataNote
+	guid	Text NOT NULL
+	mid	Int32 NOT NULL
+	mod	Int32 NOT NULL
+	usn	Int32 NOT NULL
+	tags	Text NOT NULL
+	flds	Text NOT NULL
+	sfld	Int32 NOT NULL
+	csum	Int32 NOT NULL
+	flags	Int32 NOT NULL
+	data	Text NOT NULL
+DataCard
+	nid	Int32 NOT NULL
+	did	Int32 NOT NULL
+	ord	Int32 NOT NULL
+	mod	Int32 NOT NULL
+	usn	Int32 NOT NULL
+	type	Int32 NOT NULL
+	queue	Int32 NOT NULL
+	due	Int32 NOT NULL
+	ivl	Int32 NOT NULL
+	factor	Int32 NOT NULL
+	reps	Int32 NOT NULL
+	lapses	Int32 NOT NULL
+	left	Int32 NOT NULL
+	odue	Int32 NOT NULL
+	odid	Int32 NOT NULL
+	flags	Int32 NOT NULL
+	data	Text NOT NULL
+RevLog
+	cid	Int32 NOT NULL
+	usn	Int32 NOT NULL
+	ease	Int32 NOT NULL
+	ivl	Int32 NOT NULL
+	lastIvl       Int32 NOT NULL
+	factor	Int32 NOT NULL
+	time	Int32 NOT NULL
+	type	Int32 NOT NULL
+Graves
+	usn	Int32 NOT NULL
+	oid	Int32 NOT NULL
+	type	Int32 NOT NULL
+	Primary usn oid
+|]
